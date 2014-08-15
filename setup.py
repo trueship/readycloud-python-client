2344b78e99a72601df7ab0d855bffb6bc82da23b
Taras Lyapun <taraslyapun@gmail.com>
2014-08-15 14:32:57 +0300

    Fixed classifier

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     zip_safe=False,
     keywords='readycloud-python-client',
     classifiers=[
-        'Development Status :: 2 - Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
