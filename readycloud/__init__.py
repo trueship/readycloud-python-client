2177f304318d40c400c2af2c6235a5025e596d30
Taras Lyapun <taraslyapun@gmail.com>
2014-08-16 07:40:26 +0300

    Created new version

--- readycloud/__init__.py
+++ readycloud/__init__.py
@@ -8,6 +8,6 @@ Module which exposes public api.
 
 __author__ = 'Trueship Inc'
 __email__ = 'tlyapun@trueship.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from .readycloud import ReadyCloud
