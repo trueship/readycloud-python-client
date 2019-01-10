cf2af89ee2aad83916f336e443ab019aa7753676
Vladimir Tsyupko <vladimir.tsyupko@gmail.com>
2019-01-10 18:05:24 +0200

    Bumped the version to 0.3.2.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='readycloud',
-    version='0.3.1',
+    version='0.3.2',
     description='Python client for ReadyCloud API.',
     long_description=readme + '\n\n' + history,
     author='TrueShip, LLC',
